8175a36acfad5aedf5c7114e6729e0119d276587
Evgenii Ryshkov <eryshkov@gmail.com>
2019-06-01 23:39:11 +0300

    PutUserToQueueController.php fixed

--- src/Controller/PutUserToQueueController.php
+++ src/Controller/PutUserToQueueController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PutUserToQueueController extends AbstractController
@@ -10,11 +11,13 @@ class PutUserToQueueController extends AbstractController
     /**
      * @Route("/queue/put/user/", name="app_put_user_to_queue", methods={"POST"})
      */
-    public function index()
+    public function index(Request $request)
     {
+        $data = $request->getContent();
+        
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PutUserToQueueController.php',
+            'received' => $data,
         ]);
     }
 }
