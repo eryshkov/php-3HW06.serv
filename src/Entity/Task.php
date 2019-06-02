dc27610ffc2ddcf12c12f5972aee1d09b7274188
Evgenii Ryshkov <eryshkov@gmail.com>
2019-06-02 12:30:07 +0300

    Task.php added

---
+++ src/Entity/Task.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="tasks")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $templateName;
+
+    /**
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private $templateParameters = [];
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creationDate;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $completionDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getTemplateName(): ?string
+    {
+        return $this->templateName;
+    }
+
+    public function setTemplateName(string $templateName): self
+    {
+        $this->templateName = $templateName;
+
+        return $this;
+    }
+
+    public function getTemplateParameters(): ?array
+    {
+        return $this->templateParameters;
+    }
+
+    public function setTemplateParameters(?array $templateParameters): self
+    {
+        $this->templateParameters = $templateParameters;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCompletionDate(): ?\DateTimeInterface
+    {
+        return $this->completionDate;
+    }
+
+    public function setCompletionDate(?\DateTimeInterface $completionDate): self
+    {
+        $this->completionDate = $completionDate;
+
+        return $this;
+    }
+}
